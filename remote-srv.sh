920545c5279aae06f862531803431c84b13e9858
Yauheni Kundo <ekundo@gmail.com>
2016-03-08 17:56:21 +0300

    shell script update

--- remote-srv.sh
+++ remote-srv.sh
@@ -1,5 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-until /usr/bin/python_root $DIR/remote-srv.py; do
+until /usr/bin/python $DIR/remote-srv.py; do
 	echo "Server crashed with exit code $?.  Respawning.." >&2
 	sleep 1
 done
